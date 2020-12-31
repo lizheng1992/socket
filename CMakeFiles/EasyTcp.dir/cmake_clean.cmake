44b3c3dafebfac840eebac60942c2c13543f84c4
lizheng <lizheng6@100tal.com>
2020-12-31 13:57:15 +0800

    first commit

---
+++ CMakeFiles/EasyTcp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EasyTcp.dir/main.cpp.o"
+  "EasyTcp"
+  "EasyTcp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EasyTcp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
